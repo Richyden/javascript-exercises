7ab6122b2f32915d0644a91047c51873123aefc9
Richard Denys <richardn.denys@gmail.com>
2021-02-05 17:05:42 +0100

    01-base 01-02-03

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,8 @@
+var rep = prompt("Do you want some cake ?");
+
+if (rep == "yes" || rep == "Yes") {
+    alert("Fools, the cake is a lie!");
+}
+else{
+    alert("Hum, okay. More cake for me then :p!");
+}
