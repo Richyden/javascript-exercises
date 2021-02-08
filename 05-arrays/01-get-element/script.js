4efd060c1f5c61f455b7aff861a972e71589701a
Richard Denys <richardn.denys@gmail.com>
2021-02-08 12:05:32 +0100

    dos 05-array + exo 1

---
+++ 05-arrays/01-get-element/script.js
@@ -0,0 +1,24 @@
+(function() {
+
+    var fruits = [
+        "apple",
+        "pear",
+        "raspberry",
+        "tomatos",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peache",
+        "grap",
+        "cherry",
+    ];
+
+    // your code here
+
+    document.getElementById("run").addEventListener("click", function() {
+        alert(fruits[3]);
+    })
+
+})();
